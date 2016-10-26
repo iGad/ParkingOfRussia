2a65746be8aa2775b1a04c549dd3069f5cddece4
Pavel <pavel.odegov@gmail.com>
2016-10-26 08:38:25 +0500

    Подключены карты гугл, спрашивается разрешение на определение текущей позиции.

--- ParkingOfRussia/AppDelegate.swift
+++ ParkingOfRussia/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import GoogleMaps
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,9 +17,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        GMSServices.provideAPIKey("AIzaSyCCaiba2J1y7_QK1jokieceOmIDeATpHQg")
         // Override point for customization after application launch.
         return true
     }
+    
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
--- ParkingOfRussia/ViewController.swift
+++ ParkingOfRussia/ViewController.swift
@@ -7,19 +7,70 @@
 //
 
 import UIKit
+import GoogleMaps
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    let locationManager = CLLocationManager()
+    @IBOutlet weak var mapView: GMSMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.locationManager.delegate = self
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        self.locationManager.distanceFilter = 100
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        let camera = GMSCameraPosition.camera(withLatitude: 58.013445,
+                                                          longitude: 56.248701, zoom: 10)
+        self.mapView.camera = camera
+        //self.locationManager.startUpdatingLocation()
+        
+        //5
+        self.mapView.isMyLocationEnabled = true
+        self.mapView.settings.myLocationButton = true
+        
+        //self.mapView.frame = CGRect.zero
+        //let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        //mapView.isMultipleTouchEnabled = true
+        //mapView.isMyLocationEnabled = true
+        //self.view = mapView
+        //self.view.layoutIfNeeded()
+        
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2DMake(58.013445, 56.248701)
+        marker.title = "Sydney"
+        marker.snippet = "Australia"
+        marker.map = mapView
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // MARK: - CLLocationManagerDelegate
+    private func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        // 3
+        if status == .authorizedWhenInUse {
+            
+            // 4
+            self.locationManager.startUpdatingLocation()
+            
+            //5
+            self.mapView.isMyLocationEnabled = true
+            self.mapView.settings.myLocationButton = true
+        }
     }
-
-
+    
+    // 6
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            
+            // 7
+            mapView.camera = GMSCameraPosition(target: location.coordinate, zoom: 15, bearing: 0, viewingAngle: 0)
+            
+            // 8
+            locationManager.stopUpdatingLocation()
+        }
+        
+    }
 }
+
 
